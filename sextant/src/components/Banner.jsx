86bfd273d70a4b2b48de29bf68a669eadfa31bc0
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-07-02 15:03:28 +0300

    task2

---
+++ sextant/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <header>
+      <h1>SEXTANT</h1>
+    </header>
+  );
+};
+
+export { Banner };
---
+++ sextant/src/components/Exhibit.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Exhibit = () => {
+  return (
+    <div className="exhibit">
+      <header>
+        <h3>Header</h3>
+      </header>
+      <p>Content</p>
+    </div>
+  );
+};
+
+export { Exhibit };
